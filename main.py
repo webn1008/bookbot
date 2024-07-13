f4855a5bb1d2879e00e7e51a67d7e8de14610af2
Tony Melvin <webn1008@gmail.com>
2024-07-13 13:20:06 -0400

    New changes

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
+def main():
+    with open("books/Frankenstein.txt") as f:
+        file_contents = f.read()
+print(file_contents)
