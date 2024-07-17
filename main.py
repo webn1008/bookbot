f38920a61462a25d794b7b99c9a6a1b08e1c8654
webn1008 <webn1008@gmail.com>
2024-07-17 00:11:52 -0400

    added word count function

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-def main():
-    with open("books/Frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
-main()
+import string
+
+
+def book_bot():
+
+    def main():
+        with open("books/Frankenstein.txt") as f:
+            file_contents = f.read()
+        print(file_contents)
+
+    def word_count(string):
+        with open("books/Frankenstein.txt") as f:
+            file_contents = f.read()
+        words = file_contents.split()
+        print(len(words))
+    main()
+    word_count('string')
+
+book_bot()
