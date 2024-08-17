36833da8765aafcc20c389e9885cda149baa0cf7
Tony Melvin <webn1008@gmail.com>
2024-08-17 11:43:01 -0400

    Completely refactored code.

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
 def main():
+    book_path = "books/Frankenstein.txt"
+    file_contents = book_bot(book_path)
+    num_words = word_count(file_contents)
+    chars_dict = get_chars_dict(file_contents)
+    chars_list_sorted = chars_dict_to_sorted_list(chars_dict)
 
-    def book_bot():
-        with open("books/Frankenstein.txt") as f:
-            file_contents = f.read()
-        print(file_contents)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
 
-    def word_count(string):
-        with open("books/Frankenstein.txt") as f:
-            file_contents = f.read()
-        words = file_contents.split()
-        print(len(words))
+    for item in chars_list_sorted:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
 
-    def character_count():
-        char_dict = {}
+    print("--- end report ---")
 
-        with open("books/Frankenstein.txt") as f:
-            file_contents = f.read()
 
-        for char in file_contents:
-            lower_case_char = char.lower()
-            if lower_case_char.isalpha():
-                if lower_case_char in char_dict:
-                    char_dict[lower_case_char] += 1
-                else:
-                    char_dict[lower_case_char] = 1
-        return char_dict
 
+def word_count(file_contents):
+    words = file_contents.split()
+    return len(words)
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
     char_list = []
-    char_list.append(character_count())
-    char_list.sort(reverse=True)
-    print(char_list)
-    # book_bot()
-    word_count('string')
-    character_count()
+    for ch in num_chars_dict:
+        char_list.append({"char":ch, "num": num_chars_dict[ch]})
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
+def get_chars_dict(file_contents):
+    chars = {}
+    for c in file_contents:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def book_bot(path):
+    with open(path) as f:
+        return f.read()
 
 main()
